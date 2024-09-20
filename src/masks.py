c56dd2d85781f187daa4a664fcd6c7b5f3f4e545
Yuliya_Dadashova <dya0305@yandex.ru>
2024-09-20 15:44:55 +0300

    finally homework 9.2

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,14 @@
-def get_mask_card_number(numbers: int) -> str:
+from typing import Union
+
+
+def get_mask_card_number(numbers: Union[str, int]) -> str:
     """Функция, создающая маску для номера карты в формате ХХХХ ХХ** **** ХХХХ"""
     numbers_str = str(numbers)
 
     return f"{numbers_str[:4]} {numbers_str[5:7]}** **** {numbers_str[-4:]}"
 
 
-def get_mask_account(numbers: int) -> str:
+def get_mask_account(numbers: Union[str, int]) -> str:
     """Функция, создающая маску для номера счета в формате **ХХХХ"""
     numbers_str = str(numbers)
 
--- src/widget.py
+++ src/widget.py
@@ -10,13 +10,13 @@ def mask_account_card(account: str) -> str:
         if i.isalpha():
             mask += i + " "
         elif i.isdigit() and len(i) == 16:
-            mask += get_mask_card_number (i)
+            mask += get_mask_card_number(i)
         elif i.isdigit() and len(i) == 20:
-            mask += get_mask_account (i)
+            mask += get_mask_account(i)
 
     return mask
 
 
-def get_date (str_data: str) -> str:
+def get_date(str_data: str) -> str:
     """Функция, форматирующая строку даты"""
     return f"{str_data[8:10]}.{str_data[5:7]}.{str_data[:4]}"
