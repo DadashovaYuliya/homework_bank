8af0fba7c9cd833f02c23b9dfce0ffbc08a1b4a2
Yuliya_Dadashova <dya0305@yandex.ru>
2024-09-28 20:44:35 +0300

    correction func filter-by-state

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
 def filter_by_state(new_dict: list, state: str = "EXECUTED") -> list:
-    ''' 'Функция, фильтрующая список словарей по ключу и значениею state = "EXECUTED"'''
+    """'Функция, фильтрующая список банковских операций по заданному статусу"""
     filter_dict = []
     for i in new_dict:
-        if i["state"] == "EXECUTED":
+        if i.get("state") == state:
             filter_dict.append(i)
     return filter_dict
 
 
 def sort_by_date(new_dict: list, sort_order: bool = True) -> list:
-    """'Функция, сортирующая список по дате возрастания или убывания"""
+    """'Функция, сортирующая список банковских операций по дате возрастания или убывания"""
     return sorted(new_dict, key=lambda x: x["date"], reverse=sort_order)
