0baa3ec4a7ccdb3a31ab9fd7f58d33375644d31c
Yuliya_Dadashova <dya0305@yandex.ru>
2024-09-28 15:10:05 +0300

    add funct sort-by-date

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,12 @@
-def filter_by_state (new_data: list, state = 'EXECUTED') -> list:
-    ''''Функция, фильтрующая список словарей по ключу и значениею state = "EXECUTED"'''
+def filter_by_state(new_dict: list, state: str = "EXECUTED") -> list:
+    ''' 'Функция, фильтрующая список словарей по ключу и значениею state = "EXECUTED"'''
     filter_dict = []
-    for i in new_data:
-        if i['state'] =='EXECUTED':
+    for i in new_dict:
+        if i["state"] == "EXECUTED":
             filter_dict.append(i)
     return filter_dict
+
+
+def sort_by_date(new_dict: list, sort_order: bool = True) -> list:
+    """'Функция, сортирующая список по дате возрастания или убывания"""
+    return sorted(new_dict, key=lambda x: x["date"], reverse=sort_order)
