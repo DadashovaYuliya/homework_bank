0f07f877d49e30fcf211d5afb7477fb810879405
Yuliya_Dadashova <dya0305@yandex.ru>
2024-09-30 15:59:16 +0300

    add test-masks

--- src/masks.py
+++ src/masks.py
@@ -4,12 +4,14 @@ from typing import Union
 def get_mask_card_number(numbers: Union[str, int]) -> str:
     """Функция, создающая маску для номера карты в формате ХХХХ ХХ** **** ХХХХ"""
     numbers_str = str(numbers)
-
-    return f"{numbers_str[:4]} {numbers_str[5:7]}** **** {numbers_str[-4:]}"
+    if len(numbers_str) == 16:
+        return f"{numbers_str[:4]} {numbers_str[4:6]}** **** {numbers_str[-4:]}"
+    return 'Проверьте длину номера карты'
 
 
 def get_mask_account(numbers: Union[str, int]) -> str:
     """Функция, создающая маску для номера счета в формате **ХХХХ"""
     numbers_str = str(numbers)
-
-    return f"**{numbers_str[-4:]}"
+    if len(numbers_str) == 20:
+        return f"**{numbers_str[-4:]}"
+    return 'Проверьте длину номера счета'
