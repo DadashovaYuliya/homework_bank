18d50fe7051156691629c2ff94f3af559bfd625b
Yuliya_Dadashova <dya0305@yandex.ru>
2024-10-04 21:49:59 +0300

    finaly homework 11-1

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -12,12 +13,12 @@ from src.masks import get_mask_account, get_mask_card_number
         (2504706942057145, "2504 70** **** 7145"),
     ],
 )
-def test_get_mask_card_number(numbers: Union[str, int], masks: str):
+def test_get_mask_card_number(numbers: Union[str, int], masks: str) -> None:
     """Положительные тесты на маскировку номера счета"""
     assert get_mask_card_number(numbers) == masks
 
 
-def test_get_mask_card_number_len():
+def test_get_mask_card_number_len() -> None:
     """Тесты на некорректную длину или отсутствие ввода"""
     assert get_mask_card_number("700079228960636125") == "Проверьте длину номера карты"
     assert get_mask_card_number("") == "Проверьте длину номера карты"
@@ -27,12 +28,12 @@ def test_get_mask_card_number_len():
     "account, masks",
     [("64686473678894779589", "**9589"), ("35383033474447895560", "**5560"), (73654108430135874305, "**4305")],
 )
-def test_get_mask_account(account: Union[str, int], masks: str):
+def test_get_mask_account(account: Union[str, int], masks: str) -> None:
     """Положительные тесты на маскировку номера счета"""
     assert get_mask_account(account) == masks
 
 
-def test_get_mask_account_len():
+def test_get_mask_account_len() -> None:
     """Тесты на некорректную длину или отсутствие ввода"""
     assert get_mask_account("7000792289606361254578") == "Проверьте длину номера счета"
     assert get_mask_account("") == "Проверьте длину номера счета"
