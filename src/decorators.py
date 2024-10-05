08c3da54c02c4041083e8c710a2a6fa6c117c1fa
Yuliya_Dadashova <dya0305@yandex.ru>
2024-10-05 17:59:58 +0300

    add decorators

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+from collections.abc import Callable
+from functools import wraps
+from typing import Union, Any
+
+
+def log(filename: str | None = None) -> Callable:
+    def my_decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(f"{func.__name__} ok\n")
+                return result
+            except Exception as e:
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(f"{func.__name__} error: {e}. Input: {args}, {kwargs}\n")
+                else:
+                    print(f"{func.__name__} error: {e}. Input: {args}, {kwargs}\n")
+        return wrapper
+    return my_decorator
+
+
+@log(filename="")
+def my_function(x: int, y: int) -> int:
+    """Функция, суммирующая два числа"""
+    return x + y
+
+
+my_function(1, '3')
